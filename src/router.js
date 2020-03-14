662188ce5f237b22d50b300013fc55faaf1a8d15
yoooshiii <k0w8z0m6y1sh3k@gmail.com>
2020-03-14 17:02:48 +0900

    遅延ローディング 追加

--- src/router.js
+++ src/router.js
@@ -1,10 +1,17 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
-import Users from './views/Users.vue';
-import UsersPosts from './views/UsersPosts.vue';
-import UsersProfile from './views/UsersProfile.vue';
-import HeadNamedview from './views/HeadNamedview.vue';
+
+// import Home from './views/Home.vue';
+// import Users from './views/Users.vue';
+// import UsersPosts from './views/UsersPosts.vue';
+// import UsersProfile from './views/UsersProfile.vue';
+// import HeadNamedview from './views/HeadNamedview.vue';
+
+const Home = ()=> import("./views/Home.vue");
+const Users = ()=> import("./views/Users.vue");
+const UsersPosts = ()=> import("./views/UsersPosts.vue");
+const UsersProfile = ()=> import("./views/UsersProfile.vue");
+const HeadNamedview = ()=> import("./views/HeadNamedview.vue");
 
 Vue.use(Router);
 
