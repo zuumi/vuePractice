8aab77009ea6dc836cdba2bbc193e871d8c710c9
yoooshiii <k0w8z0m6y1sh3k@gmail.com>
2020-03-15 14:06:58 +0900

    Synchronous processing by importing mapMutation(...map---)

--- src/views/HeadNamedview.vue
+++ src/views/HeadNamedview.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
-    <button @click=increment name="button">+1</button>
-    <button @click=decrement name="button">-1</button>
+    <button @click=increment(2) name="button">+1</button>
+    <button @click=decrement(2) name="button">-1</button>
   </div>
 </template>
 
 <script>
+import {mapMutations} from "vuex";
+
 export default {
   methods:{
-    increment(){
-      this.$store.commit("increment",1);
-    },
-    decrement(){
-      this.$store.commit("decrement",1);
-    }
+    ...mapMutations(["increment","decrement"])
+    // increment(){
+    //   this.$store.commit("increment",1);
+    // },
+    // decrement(){
+    //   this.$store.commit("decrement",1);
+    // }
   }
 }
 </script>
