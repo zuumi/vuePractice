d65256c3597c4c4a9ef66f8ef6bb66ad21a63e42
yoooshiii <k0w8z0m6y1sh3k@gmail.com>
2020-03-15 13:07:16 +0900

    global variable number&function by using state&getters

--- src/store.js
+++ src/store.js
@@ -6,5 +6,9 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state:{
     count:2
+  },
+  getters:{
+    double: state =>state.count*2,
+    triple: state =>state.count*3
   }
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
     <h1>Home</h1>
-    <p>{{count}}</p>
+    <p>{{ count }}</p>
+    <p>{{ double }}</p>
+    <p>{{ triple }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  computed:{
+  computed: {
+    double(){
+      return this.$store.getters.double;
+    },
+    triple(){
+      return this.$store.getters.triple;
+    },
     count(){
       return this.$store.state.count;
     }
