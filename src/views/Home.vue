012aeff9d55017f175167a0f6573992419d99d67
yoooshiii <k0w8z0m6y1sh3k@gmail.com>
2020-03-15 09:53:17 +0900

    vuex store

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
     <h1>Home</h1>
+    <p>{{count}}</p>
   </div>
 </template>
 
 <script>
+export default {
+  computed:{
+    count(){
+      return this.$store.state
+    }
+  }
+}
 </script>
